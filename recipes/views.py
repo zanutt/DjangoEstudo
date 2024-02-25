5235845091213419495e069705894ccfcf82ed2d
zanutt <matszg@live.com>
2024-02-24 23:47:31 -0300

    modificando com blocos do django

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/index.html')
+    return render(request, 'recipes/pages/index.html') 
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
 
