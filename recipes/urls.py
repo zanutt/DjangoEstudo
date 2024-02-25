28c41d95800760f272790ae8a186e08e25ab8b16
zanutt <matszg@live.com>
2024-02-25 20:52:03 -0300

    Adcionado um dinamismo que necessita de um polimento e um arquivo py faker para gerar teste

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 #quando usamos . contamos que estamos na mesma pasta do arquivo
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe), 
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"), 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/index.html') 
+    return render(request, 'recipes/pages/index.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    }) 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context= {
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+        })
 
